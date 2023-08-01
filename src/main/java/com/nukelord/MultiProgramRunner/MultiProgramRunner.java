40a73f24c3a4ae1c7c82b592413e39167c10137d
NukelordPvP <63741267+NukelordPvP@users.noreply.github.com>
2023-08-01 13:41:43 -0700

    made a new branch that supports all file types

--- src/main/java/com/nukelord/MultiProgramRunner/MultiProgramRunner.java
+++ src/main/java/com/nukelord/MultiProgramRunner/MultiProgramRunner.java
@@ -5,96 +5,105 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MultiProgramRunner {
+    private static final String CONFIG_FILE_PATH = "MultiProgramRunner.txt";
+    private static final String LOG_FILE_PATH = "MultiProgramRunner.log";
+
     public static void main(String[] args) {
+        List<String> files = new ArrayList<>();
+        List<Integer> delays = new ArrayList<>();
+        int globalDelay = 0;
+
         try {
-            ProgramConfig config = loadConfig("MultiProgramRunner.txt");
+            // Clear the log file at the beginning of each run
+            clearLogFile();
+            // Read the configuration file and extract files and delays
+            BufferedReader reader = new BufferedReader(new FileReader(CONFIG_FILE_PATH));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("FILE:")) {
+                    String[] parts = line.split(":");
+                    files.add(parts[1]);
+                    delays.add(Integer.parseInt(parts[2]));
+                } else if (line.startsWith("GLOBAL_DELAY:")) {
+                    globalDelay = Integer.parseInt(line.split(":")[1]);
+                }
+            }
+            reader.close();
 
-            List<JarFileEntry> jarList = config.jarList;
-            int globalDelay = config.globalDelay;
+            // Execute files with delays
+            BufferedWriter logWriter = new BufferedWriter(new FileWriter(LOG_FILE_PATH));
+            for (int i = 0; i < files.size(); i++) {
+                String file = files.get(i);
+                int delay = delays.get(i);
 
-            BufferedWriter logWriter = new BufferedWriter(new FileWriter("MultiProgramRunner.log", true));
+                // If the individual file delay is specified, use it; otherwise, use the global delay
+                int actualDelay = (delay > 0) ? delay : globalDelay;
 
-            for (JarFileEntry jarEntry : jarList) {
-                String jarFileName = jarEntry.jarFileName;
-                int jarDelay = jarEntry.delayBetweenJars;
+                logWriter.write("Executing " + file + " with delay: " + actualDelay + " seconds\n");
+                System.out.println("Executing " + file + " with delay: " + actualDelay + " seconds");
 
-                File jarFile = new File(jarFileName);
-                if (!jarFile.exists() || !jarFile.isFile()) {
-                    logWriter.write("Error: " + jarFileName + " not found or is not a valid JAR file.\n");
-                    logWriter.flush();
+                // Check if the file exists before executing
+                File fileToExecute = new File(file);
+                if (fileToExecute.exists() && fileToExecute.isFile()) {
+                    executeFile(fileToExecute);
+                } else {
+                    logWriter.write("Error: File not found " + file + "\n");
+                    System.out.println("Error: File not found " + file);
+                    // Log that the delay was skipped
+                    logWriter.write("Skipped delay for " + file + "\n");
+                    System.out.println("Skipped delay for " + file);
+                    // Skip the delay if the file is not found
                     continue;
                 }
 
-                runJar(jarFileName);
-                int delay = (jarDelay > 0) ? jarDelay : globalDelay;
-                if (delay > 0) {
-                    Thread.sleep(delay * 1000); // Convert seconds to milliseconds for Thread.sleep()
+                // Wait for the specified delay
+                try {
+                    Thread.sleep(actualDelay * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
 
             logWriter.close();
-        } catch (IOException | NumberFormatException | InterruptedException e) {
+            System.out.println("All files executed successfully!");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static ProgramConfig loadConfig(String fileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        int globalDelay = 0;
-        List<JarFileEntry> jarList = new ArrayList<>();
-
-        while ((line = reader.readLine()) != null) {
-            if (line.startsWith("GLOBAL_DELAY:")) {
-                globalDelay = Integer.parseInt(line.substring(line.indexOf(':') + 1).trim());
-            } else if (line.startsWith("JARS:")) {
-                while ((line = reader.readLine()) != null && !line.trim().isEmpty()) {
-                    String[] parts = line.split(";");
-                    String jarFileName = parts[0].trim();
-                    int jarDelay = (parts.length > 1 && !parts[1].trim().isEmpty()) ? Integer.parseInt(parts[1].trim()) : 0;
-                    jarList.add(new JarFileEntry(jarFileName, jarDelay));
-                }
-            }
-        }
-
-        reader.close();
-
-        return new ProgramConfig(jarList, globalDelay);
-    }
-
-    private static void runJar(String jarFileName) throws IOException {
-        String javaCmd = System.getProperty("java.home") + "/bin/java";
-        String command = javaCmd + " -jar " + jarFileName;
-
-        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
-
-        Process process = processBuilder.start();
+    private static void executeFile(File fileToExecute) {
         try {
-            process.waitFor();
-        } catch (InterruptedException e) {
+            if (fileToExecute.isFile()) {
+                // Get the absolute file path to handle spaces and special characters in the path
+                String absoluteFilePath = fileToExecute.getAbsolutePath();
+
+                // Execute the file using the default application association on the system
+                Process process = Runtime.getRuntime().exec(absoluteFilePath);
+
+                // Optional: Capture and log any output from the process (e.g., if the file is a command-line script)
+                BufferedReader outputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = outputReader.readLine()) != null) {
+                    System.out.println("Process Output: " + line);
+                }
+
+                // Wait for the process to finish
+                process.waitFor();
+            } else {
+                System.out.println("Error: Not a valid file to execute: " + fileToExecute.getName());
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Error occurred while executing file: " + fileToExecute.getName());
             e.printStackTrace();
         }
     }
-
-    private static class ProgramConfig {
-        List<JarFileEntry> jarList;
-        int globalDelay;
-
-        ProgramConfig(List<JarFileEntry> jarList, int globalDelay) {
-            this.jarList = jarList;
-            this.globalDelay = globalDelay;
-        }
-    }
-
-    private static class JarFileEntry {
-        String jarFileName;
-        int delayBetweenJars;
-
-        JarFileEntry(String jarFileName, int delayBetweenJars) {
-            this.jarFileName = jarFileName;
-            this.delayBetweenJars = delayBetweenJars;
+    private static void clearLogFile() {
+        try {
+            BufferedWriter logWriter = new BufferedWriter(new FileWriter(LOG_FILE_PATH));
+            logWriter.write(""); // Clear the log file by writing an empty string
+            logWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
